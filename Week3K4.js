3e17589914cb19448c9d855f5678357e96006753
Zrybea Whitfield <zrybeawhitfield@Zrybeas-MacBook-Pro.local>
2021-02-24 22:28:17 -0500

    another one bites the dust. Challenge for wed 2/24

---
+++ Week3K4.js
@@ -0,0 +1,18 @@
+// Kata: 8kyu "Pre-FizzBuzz Workout #1" https://www.codewars.com/kata/569e09850a8e371ab200000b
+// This is the first step to understanding FizzBuzz.
+//
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+//
+// Your expected output is an array of positive integers from 1 to n (inclusive).
+//
+// Your job is to write an algorithm that gets you from the input to the output.
+// tags: fundamentals
+// my solution:
+
+function preFizz(n) {
+    let output = [];
+    for(let i = 1; i <= n; i++) {
+        output.push(i);
+    }
+    return output;
+}
