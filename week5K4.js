c1c4282efafbf7a17a452780dee2b7019c96ab33
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-03-10 21:41:42 -0500

    My first 7kyu kata... *cries tears of joy*

---
+++ week5K4.js
@@ -0,0 +1,23 @@
+// Kata: 8kyu "Descending Order" https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+//
+// Examples:
+// Input: 42145 Output: 54421
+//
+// Input: 145263 Output: 654321
+//
+// Input: 123456789 Output: 987654321
+// //
+
+
+// tags: FUNDAMENTALS, FUNCTIONS, CONTROL FLOW, BASIC LANGUAGE FEATURES
+// my solution:
+
+function descendingOrder(n){
+
+let arr = Array.from(n.toString()).map(Number);
+
+ return parseInt(arr.sort((a, b) => b -a).join(""))
+
+
+}
