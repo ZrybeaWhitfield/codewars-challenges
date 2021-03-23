526e322889fe69a546847687c834b77394db1cfa
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-03-23 19:06:28 -0400

    3/23

--- week5/week5K4.js
+++ week5/week5K4.js
@@ -1,4 +1,4 @@
-// Kata: 8kyu "Descending Order" https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+// Kata: 7kyu "Descending Order" https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 //
 // Examples:
