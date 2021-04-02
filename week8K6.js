8a7c74d31ab3e89b28131450785afd8808778bb3
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-04-01 23:06:27 -0400

    Happy April 1

---
+++ week8K6.js
@@ -0,0 +1,17 @@
+// Kata: 7kyu "Thinkful - String Drills: Repeater" https://www.codewars.com/kata/52fba66badcd10859f00097e
+
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example:
+// Repeater.repeat("a", 5)
+// should return
+//
+// "aaaaa"
+
+
+// tags: FUNDAMENTALS
+// my solution:
+
+function repeater(string, n){
+  return string.repeat(n)
+}
