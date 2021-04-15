064aaebdd2077dcea3f3247a334f52802cb0900a
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-04-14 21:19:31 -0400

    4/19

--- leetcode/strings.js
+++ leetcode/strings.js
@@ -3,3 +3,15 @@
 var reverseString = function(s) {
    return s.reverse()
 }
+
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+//Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+var reverse = function(x) {
+    return parseInt(x.toString().split("").reverse().join("")) * Math.sign(x)
+
+};
+
+//getting fails at integers tyhat are above 64-bit? 
