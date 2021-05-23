aaf27a06201c2dfc2ff32970726dcafdea422fa8
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-05-22 23:33:38 -0400

    saturday commit

---
+++ week13K6.js
@@ -0,0 +1,14 @@
+// Kata: 8kyu "Basic variable assignment" https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+
+
+// tags: BUGS
+// my solution:
+
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
---
+++ week13K7.js
@@ -0,0 +1,16 @@
+// Kata: 8kyu "Return to Sanity" https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+
+
+
+
+// tags: BUGS
+// my solution:
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return results;
+}
