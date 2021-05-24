302f50e0eac97bb6631ca7df0445ad30ab94d150
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-05-23 22:58:49 -0400

    sunday push

---
+++ week13K11.js
@@ -0,0 +1,11 @@
+// Kata: 8kyu "Function 2 - squaring an argument" https://www.codewars.com/kata/523b623152af8a30c6000027
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// tags: FUNDAMENTALS
+// my solution:
+
+function square(arg){
+  return Math.pow(arg, 2)
+}
