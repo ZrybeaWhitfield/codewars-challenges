27ab1970b1f3dff244f7f3dc0048a8883a4f4bff
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-05-30 22:13:39 -0400

    anotha one

---
+++ week14K4.js
@@ -0,0 +1,14 @@
+// Kata: 8kyu "Kata Example Twist" https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+//
+// var websites = []
+
+
+
+// tags: FUNDAMENTALS
+// my solution:
+
+var websites = new Array(1000).fill("codewars")
