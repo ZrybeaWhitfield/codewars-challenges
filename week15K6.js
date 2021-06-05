2dd166e44b533ed2be732189a4eb2256195f129a
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-05 11:51:25 -0400

    saturday push

---
+++ week15K6.js
@@ -0,0 +1,9 @@
+// Kata: 8kyu "Push a hash/an object into array" https://www.codewars.com/kata/527b3cd0492b6b15250060af
+
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+// tags: BUGS, ARRAYS
+// my solution:
+
+items = []
+items.push({a: "b", c: "d"})
