ec86489f0a7916c9706fb9b4e4e545bbf7bbd601
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-07 22:36:32 -0400

    new wek who dis?

---
+++ week16K1.js
@@ -0,0 +1,11 @@
+// Kata: 8kyu "Convert boolean values to strings 'Yes' or 'No'." https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+// TComplete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// tags: FUNDAMENTALS, BOOLEANS, BEST PRACTICES
+// my solution:
+
+function boolToWord( bool ){
+  return bool? "Yes" : "No"
+
+}
