924983721e4db680448af649291920faed21c961
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-08 21:55:12 -0400

    another

---
+++ week16K2.js
@@ -0,0 +1,14 @@
+// Kata: 8kyu "Reverse List Order" https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples
+// reverseList([1,2,3,4]) == [4,3,2,1]
+// reverseList([3,1,5,4]) == [4,5,1,3]
+
+// tags: FUNDAMENTALS, LISTS, DATA STRUCTURES
+// my solution:
+
+function reverseList(list) {
+return list.reverse()
+}
