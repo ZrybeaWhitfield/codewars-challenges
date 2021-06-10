53ee351363ecf4668823a8a5f1ccf63613e6550a
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-09 22:18:19 -0400

    next one

---
+++ week16K3.js
@@ -0,0 +1,11 @@
+// Kata: 8kyu "You Can't Code Under Pressure #1" https://www.codewars.com/kata/53ee5429ba190077850011d4/
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// tags: FUNDAMENTALS, NUMBERS, INTEGERS
+// my solution:
+
+function doubleInteger(i) {
+  return i+i;
+}
