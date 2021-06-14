a00615df431e34d9e517627778c5e29eed7593c7
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-13 21:25:56 -0400

    sunday special

---
+++ week16K7.js
@@ -0,0 +1,14 @@
+// Kata: 8kyu "Grasshopper - Basic Function Fixer" https://www.codewars.com/kata/56200d610758762fb0000002
+
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+//
+// Can you help me fix the function?
+
+// tags: FUNDAMENTALS
+// my solution:
+
+function addFive(num) {
+  var total = num + 5
+  return total
+}
