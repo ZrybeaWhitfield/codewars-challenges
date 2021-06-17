5798f5ff60df8fb1ee53be22ec2169868b7cd7b9
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-06-17 19:14:55 -0400

    hello

---
+++ week17K3.js
@@ -0,0 +1,10 @@
+// Kata: 8kyu "Simple multiplication" https://www.codewars.com/kata/583710ccaa6717322c000105
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// tags: FUNDAMENTALS
+// my solution:
+
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8 : number * 9
+}
