f5d74f55e0b4b812c9e407e4817bffa37a277ddd
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-07-08 22:06:40 -0400

    today is today

---
+++ week20K4.js
@@ -0,0 +1,17 @@
+// Kata: 8kyu "Opposite number" https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
+// tags: FUNDAMENTALS
+// my solution:
+function opposite(number) {
+  return -number
+}
