10896ef853b20a381b20531ebaf487743874813e
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2023-11-13 12:16:27 -0500

    Additional

--- 2023/week3/week3K2.js
+++ 2023/week3/week3K2.js
@@ -1,5 +1,23 @@
-// Kata: 8kyu "Who is going to pay for the wall?" https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+// Kata: 8kyu "Fun with ES6 Classes #1 - People, people, people" https://www.codewars.com/kata/56f7f8215d7c12c0e7000b19
 
-// tags: FUNDAMENTALS, STRINGS
+// tags: FUNDAMENTALS, OBJECT-ORIENTED PROGRAMMING
+
 
 // my solution:
+class Person {
+    constructor(firstName = "John", lastName= "Doe", age= 0, gender="Male") {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender; 
+    }
+    
+    sayFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    
+    static greetExtraTerrestrials(raceName) {
+      return `Welcome to Planet Earth ${raceName}`
+    }
+    
+  }
--- 2023/week3/week3K3.js
+++ 2023/week3/week3K3.js
@@ -1,5 +1,14 @@
-// Kata: 8kyu "Who is going to pay for the wall?" https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+// Kata: 8kyu "Barking mad" https://www.codewars.com/kata/54dba07f03e88a4cec000caf
 
-// tags: FUNDAMENTALS, STRINGS
+// tags: FUNDAMENTALS, OBJECT-ORIENTED PROGRAMMING
 
 // my solution:
+function Dog (breed) {
+    this.breed = breed;
+    this.bark = function() {
+    return "Woof";
+    };
+  }
+  
+  var snoopy = new Dog("Beagle");
+  var scoobydoo = new Dog("Great Dane");
